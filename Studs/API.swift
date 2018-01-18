7606a8ab787aa9af1ae583823d89bfcad9459a6d
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-01-18 20:07:20 +0100

    Add API with login function

---
+++ Studs/API.swift
@@ -0,0 +1,34 @@
+//
+//  API.swift
+//  Studs
+//
+//  Created by Jonathan Berglind on 2018-01-18.
+//  Copyright © 2018 Studieresan. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct API {
+  static let baseURL = "https://studs18-overlord.herokuapp.com"
+  static let loginURL = baseURL + "/login"
+
+  /// Tries to log in the user using email and password.
+  /// Success boolean is available in completion handler.
+  static func login(email: String, password: String,
+                    completion: @escaping (Bool) -> Void) {
+    let parameters = [
+      "email": email,
+      "password": password,
+      ]
+    Alamofire.request(loginURL, method: .post, parameters: parameters,
+                      encoding: JSONEncoding.default).response { response in
+                        if let status = response.response?.statusCode {
+                          completion(status == 200)
+                        } else {
+                          completion(false)
+                        }
+    }
+  }
+}
+
