508d6ff292960aad47c14a65a2ccd9cb4e634fd5
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-01-18 20:55:09 +0100

    Add _basic_ user struc

---
+++ Studs/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Studs
+//
+//  Created by Jonathan Berglind on 2018-01-18.
+//  Copyright © 2018 Studieresan. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+  let name: String
+  let phone: String
+}
