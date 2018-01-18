5db09604447e2468c05750562d7511dfa4677705
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-01-18 17:26:07 +0100

    Create basic struct for Event data

---
+++ Studs/Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  Studs
+//
+//  Created by Jonathan Berglind on 2018-01-18.
+//  Copyright © 2018 Studieresan. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+  let id: String
+  let companyName: String
+  let date: Date
+}
+
