a1c737e2b5271f217c6078ea234c7a50bed5c6f7
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-01-31 17:18:06 +0100

    Make user model conform to Decodable

--- Studs/Models/User.swift
+++ Studs/Models/User.swift
@@ -8,7 +8,32 @@
 
 import Foundation
 
-struct User {
-  let name: String
-  let phone: String
+struct User: Decodable {
+  let id: String
+  let firstName: String?
+  let lastName: String?
+  let phone: String?
+
+  enum CodingKeys: String, CodingKey {
+    case id
+    case profile
+  }
+
+  enum ProfileCodingKeys: String, CodingKey {
+    case firstName
+    case lastName
+    case phone
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    id = try container.decode(String.self, forKey: .id)
+
+    let profile = try container.nestedContainer(keyedBy: ProfileCodingKeys.self,
+                                                forKey: .profile)
+    firstName = try profile.decode(String?.self, forKey: .firstName)
+    lastName = try profile.decode(String?.self, forKey: .lastName)
+    phone = try profile.decode(String?.self, forKey: .phone)
+  }
 }
