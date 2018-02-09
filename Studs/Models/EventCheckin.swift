06e3f4289c1dd54e5fea808bb56ed47e116c883f
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-02-09 08:22:40 +0100

    Add simple EventCheckin struct

---
+++ Studs/Models/EventCheckin.swift
@@ -0,0 +1,17 @@
+//
+//  EventCheckin.swift
+//  Studs
+//
+//  Created by Jonathan Berglind on 2018-02-08.
+//  Copyright © 2018 Studieresan. All rights reserved.
+//
+
+import Foundation
+
+struct EventCheckin {
+  let id: String
+  let eventId: String
+  let userId: String
+  let checkedInById: String
+  let checkedInAt: Date
+}
