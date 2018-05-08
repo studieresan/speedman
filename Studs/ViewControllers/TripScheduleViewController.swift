fd09cda6e6564d85c844a096fe79c7999439b50f
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-05-08 16:25:08 +0200

    Add tableview to display upcoming activities

--- Studs/ViewControllers/TripScheduleViewController.swift
+++ Studs/ViewControllers/TripScheduleViewController.swift
@@ -9,27 +9,43 @@
 import UIKit
 
 class TripScheduleViewController: UIViewController {
+  // MARK: - Outlets
+  @IBOutlet weak var tableView: UITableView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  // MARK: - Properties
+  private var activities = [TripActivity]() {
+    didSet {
+      tableView.reloadData()
     }
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
+    tableView.delegate = self
+    tableView.dataSource = self
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    Firebase.streamActivities { self.activities = $0 }
+  }
+}
 
+// MARK: - UITableViewDelegate
+extension TripScheduleViewController: UITableViewDelegate {
+}
+
+// MARK: - UITableViewDataSource
+extension TripScheduleViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+    -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+      let activity = activities[indexPath.row]
+      cell.textLabel?.text = activity.title
+      cell.detailTextLabel?.text = activity.location.address
+      return cell
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return activities.count
+  }
 }
