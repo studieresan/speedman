b826a7f5ecb30f3ef651afc70bf9088b88894342
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-05-08 13:23:09 +0200

    Add TripActivity model

---
+++ Studs/Models/TripActivity.swift
@@ -0,0 +1,42 @@
+//
+//  TripActivity.swift
+//  Studs
+//
+//  Created by Jonathan Berglind on 2018-05-07.
+//  Copyright © 2018 Studieresan. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct TripActivity {
+  enum City: String {
+    case newyork
+    case neworleans
+    case sanfrancisco
+    case vancouver
+  }
+
+  enum Category: String {
+    case drink
+    case food
+    case attraction
+    case other
+  }
+
+  let id: String
+  let city: City
+  let category: Category
+  let title: String
+  let description: String
+  let price: String
+  let location: Location
+  let createdDate: Date
+  let startDate: Date
+  let endDate: Date
+
+  struct Location {
+    let address: String
+    let coordinate: CLLocationCoordinate2D
+  }
+}
